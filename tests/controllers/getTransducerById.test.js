c0a5e182bbf27a009a22349b7cbd13f3c4138edf
MattAKeider <kderr41@gmail.com>
2024-02-08 20:41:57 -0500

    getTransducerById controller unit tests

---
+++ tests/controllers/getTransducerById.test.js
@@ -0,0 +1,60 @@
+const { getTransducerById } = require('../../controllers/transducer-controller');
+const Transducer = require('../../models/transducer');
+
+jest.mock('../../models/transducer');
+
+const transducerDocument = {
+  _id: "65c5412da9eedba40a62bf43",
+  name: "TW",
+  location: "CROCKER",
+  department: "MFM",
+  transducerType: "TV",
+  room: "201",
+  serialNumber: "212",
+  internalIdentifier: "41",
+  controlNumber: "00FB-12346",
+  dateReceived: "2024-01-22T00:00:00.000Z",
+  outOfService: false,
+  currentCondition: [],
+  __v: 0,
+  id: "65c5412da9eedba40a62bf43",
+  toObject: jest.fn()
+};
+
+const req = {
+  params: {
+    id: transducerDocument.id
+  }
+};
+
+const res = {
+  status: jest.fn((x) => x).mockReturnThis(),
+  json: jest.fn((x) => x),
+};
+
+const next = jest.fn((x) => x);
+
+describe('getTransducerById', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should get transducer', async () => {
+    Transducer.findById.mockResolvedValueOnce(transducerDocument);
+    await getTransducerById(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+  });
+
+  test('should fail to query the database', async () => {
+    Transducer.findById.mockRejectedValueOnce();
+    await getTransducerById(req, res, next);
+    expect(next).toHaveBeenCalledWith(new Error('Could not query from database'));
+  });
+
+  test('should fail to find a transducer with given id', async () => {
+    Transducer.findById.mockResolvedValueOnce(null);
+    await getTransducerById(req, res, next);
+    expect(next).toHaveBeenCalledWith(new Error('Could not find a transducer with that id.'));
+  });
+});
