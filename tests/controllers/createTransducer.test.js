d25a6722d6b3bbe771120aeb0b8c87f129a04288
MattAKeider <kderr41@gmail.com>
2024-02-08 23:13:04 -0500

    createTransducer controller unit tests

---
+++ tests/controllers/createTransducer.test.js
@@ -0,0 +1,93 @@
+const expressValidator = require('express-validator');
+
+const { createTransducer } = require('../../controllers/transducer-controller');
+const Transducer = require('../../models/transducer');
+
+jest.mock('express-validator');
+jest.mock('../../models/transducer');
+
+const transducerDocument = {
+  _id: "65c5412da9eedba40a62bf43",
+  name: "TW",
+  location: "CROCKER",
+  department: "MFM",
+  transducerType: "TV",
+  room: "201",
+  serialNumber: "212",
+  internalIdentifier: "41",
+  controlNumber: "00FB-12346",
+  dateReceived: "2024-01-22T00:00:00.000Z",
+  outOfService: false,
+  currentCondition: [],
+  __v: 0,
+  id: "65c5412da9eedba40a62bf43",
+  toObject: jest.fn()
+};
+
+const req = { 
+  body: {
+    name: 'TW',
+    location: 'CROCKER',
+    department: 'MFM',
+    room: '201',
+    transducerType: 'TV',
+    serialNumber: '212',
+    internalIdentifier: '41',
+    controlNumber: '00FB-12346',
+    dateReceived: '2024-01-22',
+    outOfService: false
+  }
+};
+
+const res = {
+  status: jest.fn((x) => x).mockReturnThis(),
+  json: jest.fn((x) => x),
+};
+
+const next = jest.fn((x) => x);
+
+describe('createTransducer', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    expressValidator.validationResult.mockImplementation(() => ({
+      isEmpty: jest.fn().mockReturnValue(true)
+    }));
+  });
+
+  test('should create a transducer', async () => {
+    Transducer.findOne.mockResolvedValueOnce(null);
+
+    await createTransducer(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledTimes(1);
+  });
+
+  test('should fail to query the database', async () => {
+    Transducer.findOne.mockRejectedValueOnce();
+    await createTransducer(req, res, next);
+    expect(next).toHaveBeenCalledWith(new Error('Could not query database.'));
+  });
+
+  test('should throw an error if invalid request body inputs given', async () => {
+    expressValidator.validationResult.mockImplementation(() => ({
+      isEmpty: jest.fn().mockReturnValue(false)
+    }));
+
+    await createTransducer(req, res, next);
+    expect(next).toHaveBeenCalledWith(new Error('Invalid input values, please check your data'));
+  });
+
+  test('should throw an error if transducer already exists', async () => {
+    Transducer.findOne.mockResolvedValueOnce(transducerDocument);
+    await createTransducer(req, res, next);
+    expect(next).toHaveBeenCalledWith(new Error('Transducer already exists with given name or serial number'));
+  });
+
+  test('should fail to write to the database', async () => {
+    Transducer.prototype.save.mockRejectedValueOnce();
+    
+    await createTransducer(req, res, next);
+    expect(next).toHaveBeenCalledWith(new Error('Failed to write to database.'));
+  });
+});
